54cadb5b87512fa7894a0fe02ca63258742ae022
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2024-01-08 13:35:26 +0100

    Change overview

--- overview.py
+++ overview.py
@@ -13,10 +13,17 @@ if __name__ == "__main__":
     OPTIONS["fit.wavelengths"] = [1.6, 2.25, 3.5, 8, 8.5,
                                   9, 10, 11.3, 12.5]*u.um
     OPTIONS["data.binning.window"] = 0.2
-    fits_files = list(fitting_dir.glob("*.fits"))
-    set_data(fits_files)
-    plot_overview(title="Data Overview",
-                  savefig="data_overview.pdf")
-    uv_plotter = Plotter(fits_files, plot_name="uv.pdf")
-    uv_plotter.add_uv(color_grouping="instrument",
-                      make_tracks=True).plot(save=True)
+    # fits_files = list(fitting_dir.glob("*.fits"))
+    # set_data(fits_files)
+    # plot_overview(title="Data Overview",
+    #               savefig="data_overview.pdf")
+    # uv_plotter = Plotter(fits_files, plot_name="uv.pdf")
+    # uv_plotter.add_uv(color_grouping="instrument",
+    #                   make_tracks=True).plot(save=True)
+    # ut_files = Path("/Users/scheuck/Data/reduced_data/hd142666/matisse/mat_tools/nband").glob("*INT.fits")
+    # uv_plotter = Plotter(list(ut_files), plot_name="uv_uts.pdf")
+    # uv_plotter.add_uv(color_grouping="file", make_tracks=True).plot(save=True)
+    files = list(Path("/Users/scheuck/Data/reduced_data/hd142666/matisse/mat_tools/nband").glob("*INT.fits"))
+    plotter = Plotter(files[0], plot_name="test.pdf")
+    unwrap = True if "nband" in str(files[0]) else False
+    plotter.add_mosaic(unwrap=unwrap).plot(error=True, save=True)
