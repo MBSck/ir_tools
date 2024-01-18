6db6e73fc8a7618d98bfefbe3382eeea2031c26b
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2024-01-18 16:20:08 +0100

    Add more stuff

--- overview.py
+++ overview.py
@@ -9,27 +9,37 @@ from ppdmod.options import OPTIONS
 
 if __name__ == "__main__":
     fitting_dir = Path("/Users/scheuck/Data/reduced_data/hd142666/fitting_data")
-    model_file = Path("/Users/scheuck/Pictures/MATISSEScienceMeeting/model.fits")
-    OPTIONS["plot.color.background"] = "black"
+    model_file = Path("/Users/scheuck/Pictures/Meetings/thomas_and_roy/2024-01-12/nsteps10000_nw100_kband/model.fits")
+    # OPTIONS["plot.color.background"] = "black"
     OPTIONS["fit.data"] = ["flux", "vis2", "t3phi"]
     OPTIONS["fit.wavelengths"] = [1.6, 2.25, 3.5, 8, 8.5,
                                   9, 10, 11.3, 12.5]*u.um
     OPTIONS["data.binning.window"] = 0.2
     fits_files = list(fitting_dir.glob("*.fits"))
-    set_data(fits_files)
-    plot_overview(savefig="data_overview.png")
-    plot_model(model_file, data_type="temperature", savefig="temperature.png")
-    plot_model(model_file, data_type="brightness", savefig="brightness.png")
-    plot_model(model_file, data_type="density", savefig="density.png")
-    plot_model(model_file, data_type="thickness", savefig="thickness.png")
-    plot_model(model_file, data_type="depth", savefig="depth.png")
+    # set_data(fits_files)
+    # plot_overview(savefig="data_overview.png")
+    plot_model(model_file, data_type="image", wavelength=2.25,
+               pixel_size=0.1, savefig="image225.png", zoom=5)
+    # plot_model(model_file, data_type="image", wavelength=3.5,
+    #            pixel_size=0.1, savefig="image35.png", zoom=10)
+    # plot_model(model_file, data_type="image", wavelength=10.,
+    #            pixel_size=0.1, savefig="image10.png")
+    # plot_model(model_file, data_type="image", wavelength=12.5,
+    #            pixel_size=0.1, savefig="image125.png")
+    # plot_model(model_file, data_type="temperature", savefig="temperature.png")
+    # plot_model(model_file, data_type="flux", savefig="flux.png")
+    # plot_model(model_file, data_type="brightness", savefig="brightness.png")
+    # plot_model(model_file, data_type="density", savefig="density.png")
+    # plot_model(model_file, data_type="thickness", savefig="thickness.png")
+    # plot_model(model_file, data_type="depth", factor=0.505, savefig="depth.png")
     # uv_plotter = Plotter(fits_files, plot_name="uv.pdf")
     # uv_plotter.add_uv(color_grouping="instrument",
     #                   make_tracks=True).plot(save=True)
     # ut_files = Path("/Users/scheuck/Data/reduced_data/hd142666/matisse/mat_tools/nband").glob("*INT.fits")
     # uv_plotter = Plotter(list(ut_files), plot_name="uv_uts.pdf")
     # uv_plotter.add_uv(color_grouping="file", make_tracks=True).plot(save=True)
-    # files = list(Path("/Users/scheuck/Data/reduced_data/hd142666/matisse/mat_tools/nband").glob("*INT.fits"))
-    # plotter = Plotter(files[0], plot_name="test.pdf")
-    # unwrap = True if "nband" in str(files[0]) else False
+    # path = Path("/Users/scheuck/Data/reduced_data/hd142666/matisse/1.7.6/mat_tools/lband")
+    # file = path / "hd_142666_2022-04-23T03_05_25:2022-04-23T02_28_06_HAWAII-2RG_FINAL_TARGET_INT.fits"
+    # plotter = Plotter(file, plot_name="test.pdf")
+    # unwrap = True if "nband" in str(file) else False
     # plotter.add_mosaic(unwrap=unwrap).plot(error=True, save=True)
--- remake_model_plots.py
+++ remake_model_plots.py
@@ -6,7 +6,6 @@ import numpy as np
 from ppdmod import custom_components
 from ppdmod import data
 from ppdmod import fitting
-from ppdmod import model
 from ppdmod import plot
 from ppdmod import utils
 from ppdmod.parameter import STANDARD_PARAMETERS, Parameter
@@ -17,14 +16,14 @@ from ppdmod.options import OPTIONS
 # os.environ["OMP_NUM_THREADS"] = "1"
 
 # TODO: Check wavelength axis for opacity interpolation.
-data.set_fit_wavelengths([3.520375, 10.001093]*u.um)
-path = Path("/Users/scheuck/Data/reduced_data/hd142666/matisse")
-fits_files = [
-    "hd_142666_2022-04-21T07_18_22:2022-04-21T06_47_05_HAWAII-2RG_FINAL_TARGET_INT.fits",
-    "hd_142666_2022-04-21T07_18_22:2022-04-21T06_47_05_AQUARIUS_FINAL_TARGET_INT.fits",
-    "hd_142666_2022-04-23T03_05_25:2022-04-23T02_28_06_HAWAII-2RG_FINAL_TARGET_INT.fits",
-    "hd_142666_2022-04-23T03_05_25:2022-04-23T02_28_06_AQUARIUS_FINAL_TARGET_INT.fits"]
-fits_files = list(map(lambda x: path / x, fits_files))
+OPTIONS["fit.data"] = ["flux", "vis", "t3phi"]
+OPTIONS["data.binning.window"] = 0.1*u.um
+# data.set_fit_wavelengths([1.6, 2.25, 3.5]*u.um)
+# data.set_fit_wavelengths([3.5]*u.um)
+data.set_fit_wavelengths([8., 9., 10., 11.3, 12.5]*u.um)
+fits_dir = Path("/Users/scheuck/Data/reduced_data/hd142666/matisse/1.7.6/mat_tools/nband/flux")
+# fits_files = list(fits_dir.glob("*.fits"))
+fits_files = [fits_dir / "hd_142666_2022-04-23T03_05_25:2022-04-23T02_28_06_AQUARIUS_FINAL_TARGET_INT_flux_avg.fits"]
 data.set_data(fits_files)
 
 wavelength_axes = list(
@@ -57,101 +56,132 @@ kappa_abs.value, kappa_abs.wavelength = opacity, wavelength_axes
 kappa_cont = Parameter(**STANDARD_PARAMETERS["kappa_cont"])
 kappa_cont.value, kappa_cont.wavelength = continuum_opacity, wavelength_axes
 
-fov, pixel_size = 220, 0.1
-dim = utils.get_next_power_of_two(fov / pixel_size)
-distance = 148.3
-
+dim, distance = 32, 148.3
 OPTIONS["model.constant_params"] = {
     "dim": dim, "dist": distance, "eff_temp": 7500,
-    "f": star_flux, "pixel_size": pixel_size,
+    "f": star_flux, "pixel_size": 0.1,
     "eff_radius": 1.75, "inner_temp": 1500,
     "kappa_abs": kappa_abs, "kappa_cont": kappa_cont}
 
+# rin = Parameter(**STANDARD_PARAMETERS["rin"])
+# rout = Parameter(**STANDARD_PARAMETERS["rout"])
+# p = Parameter(**STANDARD_PARAMETERS["p"])
+# inner_sigma = Parameter(**STANDARD_PARAMETERS["inner_sigma"])
+
+# rin.value = 1.
+# rout.value = 7.
+# p.value = 0.5
+# inner_sigma.value = 1e-3
+
+# rin.set(min=0.5, max=3)
+# rout.set(min=0.5, max=4)
+# p.set(min=0., max=1.)
+# inner_sigma.set(min=0, max=1e-2)
+
+# rout.free = True
+
+# inner_ring = {"rin": rin, "rout": rout, "inner_sigma": inner_sigma, "p": p}
+# inner_ring_labels = [f"ir_{label}" for label in inner_ring]
+
 rin = Parameter(**STANDARD_PARAMETERS["rin"])
 rout = Parameter(**STANDARD_PARAMETERS["rout"])
-
-rin.value = 1.
-rout.value = 7.
-
-rin.set(min=0.5, max=3)
-rout.set(min=1., max=4)
-rout.free = True
-
-inner_ring = {"rin": rin, "rout": rout}
-inner_ring_labels = [f"ir_{label}" for label in inner_ring]
-
-rin = Parameter(**STANDARD_PARAMETERS["rin"])
 a = Parameter(**STANDARD_PARAMETERS["a"])
 phi = Parameter(**STANDARD_PARAMETERS["phi"])
+p = Parameter(**STANDARD_PARAMETERS["p"])
+inner_sigma = Parameter(**STANDARD_PARAMETERS["inner_sigma"])
 
 rin.value = 13
 a.value = 0.5
 phi.value = 130
+p.value = 0.5
+inner_sigma.value = 1e-3
 
-rin.set(min=4, max=30)
+# NOTE: Set outer radius to be constant and calculate flux once?
+rin.set(min=1, max=30)
+p.set(min=0., max=1.)
+inner_sigma.set(min=0, max=1e-2)
 a.set(min=0., max=1.)
 phi.set(min=0, max=360)
 
-outer_ring = {"rin": rin, "a": a, "phi": phi}
+rout.free = True
+
+outer_ring = {"rin": rin, "a": a, "phi": phi,
+              "inner_sigma": inner_sigma, "p": p}
 outer_ring_labels = [f"or_{label}" for label in outer_ring]
 
-p = Parameter(**STANDARD_PARAMETERS["p"])
+# q = Parameter(**STANDARD_PARAMETERS["q"])
 pa = Parameter(**STANDARD_PARAMETERS["pa"])
 elong = Parameter(**STANDARD_PARAMETERS["elong"])
 cont_weight = Parameter(**STANDARD_PARAMETERS["cont_weight"])
-inner_sigma = Parameter(**STANDARD_PARAMETERS["inner_sigma"])
 
-p.value = 0.5
+# q.value = 0.5
 pa.value = 145
 elong.value = 0.5
 cont_weight.value = 130
-inner_sigma.value = 1e-3
 
-p.set(min=0., max=1.)
+# q.set(min=0., max=1.)
 pa.set(min=0, max=360)
 elong.set(min=0, max=1)
 cont_weight.set(min=0., max=1.)
-inner_sigma.set(min=0, max=1e-2)
 
-OPTIONS["model.shared_params"] = {"p": p, "pa": pa, "elong": elong,
-                                  "inner_sigma": inner_sigma,
+OPTIONS["model.shared_params"] = {"pa": pa, "elong": elong,
                                   "cont_weight": cont_weight}
 shared_params_labels = [f"sh_{label}"
                         for label in OPTIONS["model.shared_params"]]
 
+# OPTIONS["model.components_and_params"] = [
+#     ["Star", {}],
+#     ["AnalyticalTempGradient", inner_ring],
+#     ["AnalyticalAsymmetricTempGradient", outer_ring],
+# ]
+
 OPTIONS["model.components_and_params"] = [
     ["Star", {}],
-    ["SymmetricSDGreyBodyContinuum", inner_ring],
-    ["AsymmetricSDGreyBodyContinuum", outer_ring],
+    # ["AnalyticalGreyBody", inner_ring],
+    ["AnalyticalAsymmetricGreyBody", outer_ring],
 ]
-labels = inner_ring_labels + outer_ring_labels + shared_params_labels
 
-wavelength = OPTIONS["fit.wavelengths"][-1]
-results_dir = Path("")
-components = custom_components.assemble_components(
-    OPTIONS["model.components_and_params"], OPTIONS["model.shared_params"])
-# m = model.Model(components)
-# image = m.calculate_image(4096, 0.1, wavelength)
-# fits_filename = results_dir / "model_init.fits"
-# hdu = fits.PrimaryHDU(image.value)
-# hdu.writeto(fits_filename, overwrite=True)
-# plot.plot_model(4096, 0.1, m, wavelength, zoom=50, savefig=results_dir / "model_init.pdf")
+# labels = inner_ring_labels + outer_ring_labels + shared_params_labels
+labels = outer_ring_labels + shared_params_labels
+# labels = inner_ring_labels + shared_params_labels
 
-theta = np.load(results_dir / "best_fit_params.npy")
-components_and_params, shared_params = fitting.set_params_from_theta(theta)
-components = custom_components.assemble_components(
-    components_and_params, shared_params)
+OPTIONS["model.modulation.order"] = 1
+OPTIONS["model.gridtype"] = "logarithmic"
 
-# HACK: This is to include innermost radius for rn.
-innermost_radius = components[1].params["rin"]
-for component in components:
-    component.params["rin0"] = innermost_radius
+OPTIONS["fit.method"] = "emcee"
 
-new_params = dict(zip(labels, theta))
-m = model.Model(components)
-# plot.save_model_fits(dim, pixel_size, distance,
-#                      new_params["sh_pa"], new_params["sh_elong"],
-#                      m, wavelength, savefits=results_dir / "model.fits")
-OPTIONS["plot.color.background"] == "black"
-plot.plot_fit(new_params["sh_elong"], new_params["sh_pa"],
-              savefig=results_dir / "fit_results.png")
+result_dir = Path("/Users/scheuck/Data/model_results/2024-01-16/results_model_0:50:40")
+# OPTIONS["plot.color.background"] = "black"
+
+if __name__ == "__main__":
+    theta = np.load(result_dir / "best_fit_params.npy")
+    components_and_params, shared_params = fitting.set_params_from_theta(theta)
+    components = custom_components.assemble_components(
+            components_and_params, shared_params)
+    # component_labels = ["Star", "Inner Ring", "Outer Ring"]
+    component_labels = ["Star", "Inner Ring"]
+    # component_labels = ["Star", "Outer Ring"]
+
+    # HACK: This is to include innermost radius for rn.
+    innermost_radius = components[1].params["rin"]
+    for component in components:
+        component.params["rin0"] = innermost_radius
+
+    new_params = dict(zip(labels, theta))
+    # plot.save_fits(
+    #         4096, 0.1, distance,
+    #         OPTIONS["fit.wavelengths"], components,
+    #         component_labels, opacities=[kappa_abs, kappa_cont],
+    #         savefits=result_dir / "model.fits",
+    #         options=OPTIONS, object_name="HD 142666")
+
+    plot.plot_fit(
+            new_params["sh_elong"], new_params["sh_pa"],
+            components=components,
+            savefig=result_dir / "fit_results.pdf")
+
+    # plot.plot_fit(
+    #         new_params["sh_elong"], new_params["sh_pa"],
+    #         components=components,
+    #         savefig=result_dir / "fit_results.pdf",
+    #         wavelength_range=[8, 13]*u.um, ylimits={"vis2": [0, 0.2]})
