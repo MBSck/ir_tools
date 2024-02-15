f110e40d9886cb9e01de27296369c2a743351eed
Marten Scheuck <martenscheuck@gmail.com>
2024-02-15 15:19:20 +0100

    Try plotting

--- plot/plot.py
+++ plot/plot.py
@@ -10,8 +10,9 @@ if __name__ == "__main__":
     #     plotter = Plotter(fits_file, save_path=fits_file.parent)
     #     plotter.add_flux().plot(error=True, save=True, margin=0.3)
 
-    directory = Path("")
-    for fits_file in get_fits_by_tag(directory, "RAW_INT"):
+    directory = Path("/data/beegfs/astro-storage/groups/matisse/scheuck/data/matisse/GTO/hd142527/product/test/2021-03-08/reduced/coherent/lband/mat_raw_estimates.2021-03-08T07_25_46.HAWAII-2RG.rb")
+    fits_files = get_fits_by_tag(directory, "RAW_INT")
+    for fits_file in fits_files:
         plot_fits = Plotter(fits_file, save_path=directory)
         unwrap = True if "AQUARIUS" in str(fits_file) else False
         plot_fits.add_t3(unwrap=unwrap).add_vis().add_vis2()
