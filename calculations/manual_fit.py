814b2052b2d8e2bca808f04c5396ec0d5b6fdc52
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2024-02-27 11:12:31 +0100

    Update .gitignore

--- calculations/manual_fit.py
+++ calculations/manual_fit.py
@@ -26,9 +26,9 @@ def calc_blackbody(temperature: u.K, wavelengths: u.um, weight: u.mas) -> np.nda
     return bb*(weight**2).to(u.sr)*np.pi
 
 
-def plot_sed(sed_file: Path) -> None:
+def plot_sed(flux_dir: Path) -> Tuple[np.ndarray, np.ndarray]:
     _, axarr = plt.subplots(2, 3, figsize=(15, 10))
-    wl, flux = np.loadtxt(sed_file, unpack=True, comments="#", usecols=(1, 2))
+    wl, flux = np.loadtxt(flux_dir / "HD+142666.sed.dat", unpack=True, comments="#", usecols=(1, 2))
     wl, flux = wl*u.AA, (flux*u.erg/u.s/u.cm**2/u.AA)
     scales = [u.erg/u.s/u.cm**2/u.AA, u.erg/u.s/u.cm**2/u.um, u.erg/u.s/u.cm**2/u.um]
     labels = [r"Angstrom", "Micron", "Micron"]
@@ -65,20 +65,31 @@ def plot_sed(sed_file: Path) -> None:
 
             axarr[index_wl, index].set_xlabel(rf"$\lambda$ ({label})")
                 
-    plt.savefig("sed.pdf", format="pdf")
+    plt.savefig("sed_combined.pdf", format="pdf")
     plt.close()
+
+    wl, flux = wl.to(u.um), flux.to(u.erg/u.s/u.cm**2/u.um)
+    flux *= wl
+
+    # data = ["HD_142666_timmi2.txt", "10402847.ss"]
+    # data = ["HD_142666_timmi2.txt"]
+    data = ["HD142666_spitzer_psf.dat"]
+    for dataset in data:
+        wl_data, flux_data, *_ = np.loadtxt(flux_dir / dataset, unpack=True, comments="#", skiprows=1)
+        wl_data, flux_data = wl_data*u.um, (flux_data*u.Jy).to(u.erg/u.s/u.cm**2/u.Hz)
+        flux_data = flux_data.to(u.erg/u.s/u.cm**2, u.spectral_density(wl_data))
+        wl, flux = np.concatenate((wl, wl_data)), np.concatenate((flux, flux_data))
+
+    ind = np.argsort(wl)
+    wl, flux = wl[ind], flux[ind]
+
+    ind = np.where((wl > 1*u.um) & (wl < 14*u.um))
+    return wl[ind], flux[ind]
 
 
 if __name__ == "__main__":
     flux_dir = Path("/Users/scheuck/Data/flux_data/hd142666/")
-    sed_file = flux_dir / "HD+142666.sed.dat" 
-    # plot_sed(sed_file)
-    wl, flux = np.loadtxt(sed_file, unpack=True, comments="#", usecols=(1, 2))
-    wl, flux = (wl*u.AA).to(u.um), (flux*u.erg/u.s/u.cm**2/u.AA).to(u.erg/u.s/u.cm**2/u.um)
-    ind = np.where((wl > (1*u.um).to(u.AA)) & (wl < (14*u.um).to(u.AA)))
-    wl, flux = wl[ind], flux[ind]
-    flux *= wl
-
+    wl, flux = plot_sed(flux_dir)
     wl_star, flux_star, *_ = np.loadtxt(flux_dir / "HD142666_stellar_model.txt.gz",
                                         comments="#", unpack=True)
     wl_star *= u.um
@@ -86,16 +97,21 @@ if __name__ == "__main__":
     flux_star = np.interp(wl, wl_star, flux_star)
     flux -= flux_star
 
-    temps, ratios = [1500, 1100, 900], [0.72, 1.2, 2.95]
-    bbs = [calc_blackbody(temp*u.K, wl, ratio*u.mas) for temp, ratio in zip(temps, ratios)]
+    temps, ratios = [2100, 1500, 1100, 900, 500], [0.15, 0.68, 0.8, 3.17, 4.1]
+    wl_range = np.linspace(np.min(wl.value), np.max(wl.value), 300)
+    bbs = [calc_blackbody(temp*u.K, wl_range*u.um, ratio*u.mas) for temp, ratio in zip(temps, ratios)]
 
     nband = np.where((wl > 8.0*u.um) & (wl < 14.0*u.um))
-    inner_contribution = (np.sum(bbs, axis=0)/flux.value)[nband]*100
-    print(inner_contribution)
+    bb_combined = np.sum(bbs, axis=0)
+    bb_combined_interpn = np.interp(wl, wl_range*u.um, bb_combined)
+    inner_contribution = (bb_combined_interpn/flux.value)[nband]
+    np.save("flux_ratio_inner_disk_hd142666.npy", np.array([wl[nband], inner_contribution]))
+    print(inner_contribution*100)
     plt.scatter(wl.value, flux.value, s=5, alpha=0.6, label="Data")
+
     for index, bb in enumerate(bbs):
-        plt.plot(wl.value, bb.value, label=f"{temps[index]} K")
-    plt.plot(wl.value, np.sum(bbs, axis=0), label=f"Combined")
+        plt.plot(wl_range, bb.value, label=f"{temps[index]} K")
+    plt.plot(wl_range, bb_combined, label=f"Combined")
 
     plt.xlabel(r"Wavelength ($\mu$m)")
     plt.ylabel(r"$\lambda F_{\lambda}$ (erg s$^{-1}$ cm$^{-2}$)")
