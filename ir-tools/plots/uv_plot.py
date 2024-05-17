1679303276543a8100fddce9c9aa1286fffeca5e
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2024-05-17 15:18:52 +0200

    Add more plotting functionality

--- ir-tools/plots/uv_plot.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-import matplotlib.pyplot as plt
-from matadrs.utils.plot import Plotter
-
-data_path = Path("/Users/scheuck/Data/reduced_data/hd142666/matisse")
-files = list(data_path.glob("*AQUARIUS*"))
-plot_fits = Plotter(files)
-plot_fits.add_uv().plot(save=True, format="png")
