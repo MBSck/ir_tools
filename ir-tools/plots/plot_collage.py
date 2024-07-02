a63faac45ce98914eb59f2fb2ad484648c069956
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2024-07-02 12:37:01 +0200

    Make manual fit to estimate disk

--- ir-tools/plots/plot_collage.py
+++ ir-tools/plots/plot_collage.py
@@ -8,14 +8,15 @@ if __name__ == "__main__":
     files_N, files_L = path.glob("*AQUARIUS*"), path.glob("*HAWAII*")
 
     files = map(lambda files: map(lambda x: path / x, files), [files_L, files_N])
+    files = list(Path("/Users/scheuck/Data/reduced_data/hd142527/matisse/nband").glob("*07-01*"))
     for fits_file in files:
-        unwrap = "HAWAII" not in fits_file[0].name
+        unwrap = "HAWAII" not in fits_file.name
         plot_fits = Plotter(fits_file)
+        plot_fits.add_flux()
         plot_fits.add_vis(corr_flux=True)
-        plot_fits.add_diff_phases(unwrap=unwrap)
-        plot_fits.add_cphases(unwrap=unwrap)
-        plot_fits.plot(savefig=f"collage_{'N_band' if unwrap else 'L_band'}",
-                       legend_format="short,"
+        # plot_fits.add_diff_phases(unwrap=unwrap)
+        plot_fits.add_t3(unwrap=unwrap)
+        plot_fits.plot(legend_format="short",
                        error=True, subplots=False,
                        margin=0.3, legend_size="medium",
-                       sharex=True, share_legend=True)
+                       sharex=True, share_legend=True, save=True)
