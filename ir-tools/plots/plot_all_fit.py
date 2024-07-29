9eedd289c685df9f40c61a727804375bc9279b7f
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2024-07-29 12:11:59 +0200

    Improve plotting of fit results

--- ir-tools/plots/plot_all_fit.py
+++ ir-tools/plots/plot_all_fit.py
@@ -7,11 +7,12 @@ from ppdmod.analysis import restore_from_fits
 from ppdmod.data import set_data
 from ppdmod.options import OPTIONS
 from ppdmod.plot import plot_component_mosaic, \
-    plot_fit, plot_interferometric_observables, plot_overview, plot_sed
+    plot_fit, plot_interferometric_observables, \
+    plot_overview, plot_sed, plot_intermediate_products
 
 
 if __name__ == "__main__":
-    path = Path("/Users/scheuck/Data/model_results/2024-07-23/results_model_16:33:06")
+    path = Path("/Users/scheuck/Data/model_results/2024-07-26/results_model_11:52:07")
     fit_plot_dir = path / "fits"
     fit_plot_dir.mkdir(exist_ok=True, parents=True)
     data_plot_dir = path / "data"
@@ -30,29 +31,31 @@ if __name__ == "__main__":
                        margin=0.3, legend_size="medium",
                        sharex=True, share_legend=True, save=True)
 
-    # lband_data = list(data_dir.glob("*HAW*"))
-    # plot_fits = Plotter(lband_data[:3],
-    #                     plot_name="lband_data.pdf", save_dir=data_plot_dir)
-    # plot_fits.add_uv(uv_extent=150).add_flux().add_vis(corr_flux=True).add_t3(unwrap=False)
-    # plot_fits.plot(**plot_kwargs)
-    #
-    # plot_fits = Plotter(lband_data[4:],
-    #                     plot_name="lband_data_continuation.pdf", save_dir=data_plot_dir)
-    # plot_fits.add_uv(uv_extent=150).add_flux().add_vis(corr_flux=True).add_t3(unwrap=False)
-    # plot_fits.plot(**plot_kwargs)
-    #
-    # plot_fits = Plotter(list(data_dir.glob("*AQU*")),
-    #                     plot_name="nband_data.pdf", save_dir=data_plot_dir)
-    # plot_fits.add_uv(uv_extent=150).add_flux().add_vis(corr_flux=True).add_t3(unwrap=True)
-    # plot_fits.plot(**plot_kwargs)
-    #
+    lband_data = list(data_dir.glob("*HAW*"))
+    plot_fits = Plotter(lband_data[:3],
+                        plot_name="lband_data.pdf", save_dir=data_plot_dir)
+    plot_fits.add_uv(uv_extent=150).add_flux().add_vis(corr_flux=True).add_t3(unwrap=False)
+    plot_fits.plot(**plot_kwargs)
+
+    plot_fits = Plotter(lband_data[4:],
+                        plot_name="lband_data_continuation.pdf", save_dir=data_plot_dir)
+    plot_fits.add_uv(uv_extent=150).add_flux().add_vis(corr_flux=True).add_t3(unwrap=False)
+    plot_fits.plot(**plot_kwargs)
+
+    plot_fits = Plotter(list(data_dir.glob("*AQU*")),
+                        plot_name="nband_data.pdf", save_dir=data_plot_dir)
+    plot_fits.add_uv(uv_extent=150).add_flux().add_vis(corr_flux=True).add_t3(unwrap=True)
+    plot_fits.plot(**plot_kwargs)
+
+    dim = 1024
     wavelength = np.concatenate((wavelengths["lband"], wavelengths["mband"], wavelengths["nband"]))
     data = set_data(fits_files, wavelengths=wavelength, fit_data=["flux", "vis", "t3"])
     component_labels, components, sampler = restore_from_fits(path)
     plot_overview(savefig=data_plot_dir / "data_overview.pdf")
-    plot_component_mosaic(components, 4096, 0.1, wavelength.value, norm=0.2,
+    plot_component_mosaic(components, dim, 0.1, wavelength.value, norm=0.2,
                           savefig=fit_plot_dir / "mosaic_model.pdf", zoom=25)
     plot_fit(components=components, savefig=fit_plot_dir / "fit_results.pdf")
+    plot_intermediate_products(dim, wavelength, components, component_labels, save_dir=fit_plot_dir)
     plot_sed([1, 12]*u.um, components, scaling="nu", save_dir=fit_plot_dir)
     plot_sed([1, 12]*u.um, components, scaling="none", save_dir=fit_plot_dir)
     plot_interferometric_observables([1, 12]*u.um, components, component_labels, save_dir=fit_plot_dir)
