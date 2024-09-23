59cf17363bd4a01e2b63adc071b42d13d3738938
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2024-09-23 23:20:21 +0200

    Improve SED fit plotting

--- ir-tools/plots/plot_sed_fit.py
+++ ir-tools/plots/plot_sed_fit.py
@@ -4,7 +4,7 @@ import astropy.units as u
 import numpy as np
 from matadrs.utils.plot import Plotter
 from ppdmod.analysis import save_fits, restore_from_fits
-from ppdmod.fitting import compute_chi_sq, get_best_fit
+from ppdmod.fitting import compute_sed_chi_sq, get_best_fit
 from ppdmod.data import set_data, get_all_wavelengths
 from ppdmod.options import OPTIONS
 from ppdmod.plot import plot_component_mosaic, \
@@ -16,7 +16,7 @@ def ptform():
 
 
 if __name__ == "__main__":
-    path = Path("/Users/scheuck/Data/model_results/sed_fits/2024-09-19/results_model_13:32:04")
+    path = Path("/Users/scheuck/Data/model_results/sed_fits/2024-09-23/only_low")
     fit_plot_dir = path / "fits"
     fit_plot_dir.mkdir(exist_ok=True, parents=True)
     data_plot_dir = path / "data"
@@ -24,7 +24,16 @@ if __name__ == "__main__":
 
     OPTIONS.model.output = "non-normed"
     data_dir = Path("/Users/scheuck/Data/fitting_data/hd142527")
-    fits_file = [data_dir / "sed_fit" / "hd142527_average_sed.fits"]
+    # fits_dir = data_dir / "sed_fit"
+    # fits_dir = data_dir / "sed_fit" / "downsampled"
+    # fits_dir = data_dir / "sed_fit" / "only_high"
+    fits_dir = data_dir / "sed_fit" / "only_low"
+
+    # wavelength_range = None
+    wavelength_range = [8., 13.1] * u.um
+    data = set_data(list(fits_dir.glob("*fits")), wavelengths="all",
+                wavelength_range=wavelength_range, fit_data=["flux"])
+    wavelengths = get_all_wavelengths()
 
     plot_kwargs = dict(legend_format="short",
                        error=True, subplots=True,
@@ -33,10 +42,6 @@ if __name__ == "__main__":
 
     # plot_fits = Plotter(fits_file, plot_name="nband_data.pdf", save_dir=data_plot_dir)
     # plot_fits.add_uv(uv_extent=150).add_flux().plot(**plot_kwargs)
-
-    data = set_data(fits_file, wavelengths="all",
-                    wavelength_range=[8., 13.15] * u.um,
-                    fit_data=["flux"])
 
     labels, units = np.load(path / "labels.npy").tolist(), np.load(path / "units.npy", allow_pickle=True)
     component_labels, components, sampler = restore_from_fits(path)
@@ -47,11 +52,8 @@ if __name__ == "__main__":
     indices = list(map(labels.index, filter(lambda x: "weight" in x and "pah" not in x, labels)))
     print(f"Normed sum: {np.array(theta)[indices].sum()}")
 
-    model_flux = components[0].compute_flux(get_all_wavelengths())
-    chi_sq = compute_chi_sq(
-        data.flux.value, data.flux.err, model_flux, func_method="default")
-    nfree_params = len(components[0].get_params(free=True))
-    rchi_sq = chi_sq / (data.flux.value.size - nfree_params)
+    rchi_sq = compute_sed_chi_sq(
+        components[0].compute_flux(get_all_wavelengths()), reduced=True)
     print(f"rchi_sq: {rchi_sq:.2f}")
 
     # TODO: Save these as well in the fits file? Maybe even the sampler?
@@ -59,7 +61,7 @@ if __name__ == "__main__":
     # plot_chains(sampler, labels, units, savefig=fit_plot_dir / "chains.pdf")
 
     dim = 1024
-    # plot_overview(savefig=data_plot_dir / "data_overview.pdf")
+    plot_overview(savefig=data_plot_dir / "data_overview.pdf")
     # plot_fit(components=components, savefig=fit_plot_dir / "fit_results.pdf")
     # plot_component_mosaic(components, dim, 0.1, norm=0.2,
     #                       savefig=fit_plot_dir / "mosaic_model.pdf", zoom=8)
