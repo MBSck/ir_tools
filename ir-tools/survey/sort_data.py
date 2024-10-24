fe430f0f67733cb748c2e5b95655e6f4df65f572
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2024-10-24 18:47:53 +0200

    Include calculating `OI_VIS2` for Varga reductions

--- ir-tools/survey/sort_data.py
+++ ir-tools/survey/sort_data.py
@@ -4,11 +4,31 @@ from datetime import datetime, timedelta
 from pathlib import Path
 from typing import List
 
+import numpy as np
 import pandas as pd
+from astropy.io import fits
 from tqdm import tqdm
+from uncertainties import unumpy
 
 
-def get_target(sheet: Path, target: str) -> pd.DataFrame:
+def correct_vis_to_vis2(fits_file: Path) -> None:
+    """The Varga reduction save "OI_VIS" in "OI_VIS2", this corrects it
+    to be "OI_VIS2" to be oimodeler compliant."""
+    with fits.open(fits_file, mode="update") as hdul:
+        for card in hdul:
+            header = card.header
+            if "EXTNAME" not in header:
+                continue
+
+            if header["EXTNAME"] == "OI_VIS2":
+                vis = unumpy.uarray(card.data["VIS2DATA"], np.abs(card.data["VIS2ERR"]))
+                card.data["VIS2DATA"] = unumpy.nominal_values(vis**2)
+                card.data["VIS2ERR"] = unumpy.std_devs(vis**2)
+
+        hdul.flush()
+
+
+def get_source(sheet: Path, target: str) -> pd.DataFrame:
     """Gets the night a target was observed for some criteria."""
     df = pd.read_excel(sheet, skiprows=1, header=[0, 1])
     target_column = df["Target"].iloc[:, 0].astype(str)
@@ -18,28 +38,46 @@ def get_target(sheet: Path, target: str) -> pd.DataFrame:
     ]
 
 
-def get_target_list(
+def get_source_list(
     excel_file: Path, sheet_name: str = "target_list_status"
 ) -> List[str]:
+    """Gets the list of source names to sort from the excel file."""
     df = pd.read_excel(excel_file, sheet_name=sheet_name, header=[0, 1])
     nband_selection = df["Selected"]["for LN paper"].astype(str)
     df_filtered = df[nband_selection != "no"]
     df_cleaned = df_filtered.dropna(subset=[("Selected", nband_selection.name)])
     lband_selection = df_cleaned["Selected"]["for L paper"].astype(str)
-    df_final =  df_cleaned[~lband_selection.isin(["no", "tbd"])]
+    df_final = df_cleaned[~lband_selection.isin(["no", "tbd"])]
     return df_final["Name"].iloc[:, 0].tolist()
 
 
-def get_dir_name(target: str) -> str:
-    """Returns the directory name for a target."""
-    if " " in target:
-        return target.replace(" ", "_")
-    if target.startswith("HD"):
-        return target[:2] + "_" + target[2:]
+def get_dir_name(source: str) -> str:
+    """Returns the directory name for a source from its excel sheet name."""
+    if " " in source:
+        return source.replace(" ", "_")
+
+    if source.startswith("HD"):
+        return source[:2] + "_" + source[2:]
+
+    return source
 
 
 def get_date(night: str, nights: pd.DataFrame) -> str | None:
-    """Returns the date of the night the target was observed."""
+    """Compares the obsevation dates of the file name to the one found
+    in the excel sheet and returns the one found in the file name.
+
+    Parameters
+    ----------
+    night : str
+        The night to compare.
+    nights : pd.DataFrame
+        The nights found in the excel sheet.
+
+    Returns
+    -------
+    date :  str
+        The date found in the file name.
+    """
     night = datetime.strptime(night, "%Y-%m-%d")
     night_before = (night - timedelta(days=1)).strftime("%Y-%m-%d")
     night_after = (night + timedelta(days=1)).strftime("%Y-%m-%d")
@@ -57,9 +95,21 @@ def get_date(night: str, nights: pd.DataFrame) -> str | None:
     return key
 
 
-def sort_target(sheet: Path, target: str, source_dir: Path, target_dir: Path) -> None:
-    """Sorts the data for a target."""
-    dir_name = get_dir_name(target)
+def sort_target(sheet: Path, source: str, source_dir: Path, target_dir: Path) -> None:
+    """Sorts the data for a target.
+
+    Parameters
+    ----------
+    sheet : Path
+        The excel file containing the information about the observations.
+    source : str
+        The source for which to sort the data.
+    source_dir : Path
+        The directory containing the directories with the targets.
+    target_dir : Path
+        The directory where to save the sorted data.
+    """
+    dir_name = get_dir_name(source)
     target_dir /= dir_name
     if target_dir.exists():
         shutil.rmtree(target_dir)
@@ -68,7 +118,7 @@ def sort_target(sheet: Path, target: str, source_dir: Path, target_dir: Path) ->
         quality_dir = target_dir / quality
         quality_dir.mkdir(parents=True, exist_ok=True)
 
-    df = get_target(sheet, target)
+    df = get_source(sheet, source)
     nights = df["Night"].iloc[:, 0].apply(lambda x: x.strftime("%Y-%m-%d")).to_dict()
     reversed_nights = {v: k for k, v in nights.items()}
 
@@ -79,15 +129,14 @@ def sort_target(sheet: Path, target: str, source_dir: Path, target_dir: Path) ->
             continue
 
         key = get_date(date.group(), nights)
-        if key is None:
-            shutil.copy(fits_file, target_dir / "tba" / fits_name)
-        else:
+        new_fits_file = target_dir / "tba" / fits_name
+        if key is not None:
             row = df.loc[reversed_nights[key]]
             config = row["Data description"]["Tel. config."]
             band = "L" if any(letter == "L" for letter in fits_name.split("_")) else "N"
 
             if band == "N" and "at" in config.lower():
-                shutil.copy(fits_file, target_dir / "tba" / fits_name)
+                new_fits_file = target_dir / "tba" / fits_name
             else:
                 quality = str(row["Data quality"][f"{band} band"])
                 if "good" in quality:
@@ -99,7 +148,10 @@ def sort_target(sheet: Path, target: str, source_dir: Path, target_dir: Path) ->
                 else:
                     quality = "tba"
 
-                shutil.copy(fits_file, target_dir / quality / fits_name)
+                new_fits_file = target_dir / quality / fits_name
+
+        shutil.copy(fits_file, new_fits_file)
+        correct_vis_to_vis2(new_fits_file)
 
 
 if __name__ == "__main__":
@@ -108,5 +160,5 @@ if __name__ == "__main__":
     source_dir = data_dir / "reduced_data" / "jozsef_reductions" / "targets5"
     target_dir = data_dir / "survey"
 
-    for target in tqdm(get_target_list(excel_file), "Sorting data..."):
+    for target in tqdm(get_source_list(excel_file), "Sorting data..."):
         sort_target(excel_file, target, source_dir, target_dir)
