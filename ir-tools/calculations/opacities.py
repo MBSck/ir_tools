3842c06d32697c4dc7068f39116e73ab66f743f4
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2024-10-28 13:36:26 +0100

    Update `opacities`

--- ir-tools/calculations/opacities.py
+++ ir-tools/calculations/opacities.py
@@ -7,23 +7,27 @@ SHORTNAMES = ["pyrox", "enst", "forst", "sil", "oliv"]
 NAMES = ["Pyroxene", "Enstatite", "Forsterite", "Silica", "Olivine"]
 NAMES_DICT = dict(zip(SHORTNAMES, NAMES))
 
+
+def generate_opacity_file(
+    data_dir: Path,
+    weights: np.ndarray,
+    method: str = "grf",
+):
+    wl, opacity = get_opacity(data_dir / "opacities", weights, NAMES, method)
+    np.save(
+        data_dir / "opacities" / f"hd142527_silicate_{method}_opacities.npy",
+        np.array([wl, opacity]),
+    )
+
+
 if __name__ == "__main__":
     data_dir = Path().home() / "Data"
     path = data_dir / "model_results" / "sed_fits" / "2024-10-24"
     dir_name = "averaged"
 
-    labels, weights = np.load(
+    _, weights = np.load(
         path / dir_name / "assets" / "silicate_labels_and_weights.npy"
     )
     weights = weights.astype(float) / weights.astype(float).sum()
-
-    names = []
-    for label in [NAMES_DICT[label.split("_")[1]] for label in labels]:
-        if label not in names:
-            names.append(label)
-
-    wl, opacity = get_opacity(data_dir / "opacities", weights, names, "grf")
-    np.save(
-        data_dir / "opacities" / "hd142527_silicate_opacities.npy",
-        np.array([wl, opacity]),
-    )
+    # weights = np.array([0, 73.2, 0.6, 14.2, 8.6, 0, 2.4, 1.0, 0, 0]) / 1e2
+    generate_opacity_file(data_dir, weights, method="grf")
