a938f1070f98a40040692dc320d3ae5a85f77222
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2024-10-30 12:01:11 +0100

    Make empirial opacity files with smoothing at the edges

--- ir-tools/calculations/opacities.py
+++ ir-tools/calculations/opacities.py
@@ -1,13 +1,24 @@
 from pathlib import Path
+from typing import Tuple
 
 import astropy.units as u
 import matplotlib.pyplot as plt
 import numpy as np
 from astropy.io import fits
-from ppdmod.utils import get_opacity
+from ppdmod.utils import get_opacity, load_data, qval_to_opacity
+from astropy.modeling.models import BlackBody
 
-SHORTNAMES = ["pyrox", "enst", "forst", "sil", "oliv"]
 NAMES = ["Pyroxene", "Enstatite", "Forsterite", "Silica", "Olivine"]
+
+
+def interpolation_smooth(
+    grid: np.ndarray, value: np.ndarray, indices: Tuple[int, int]
+) -> np.ndarray:
+    return np.interp(
+        grid[slice(*indices)],
+        [grid[indices[0]], grid[indices[-1]]],
+        [value[indices[0]], value[indices[-1]]],
+    )
 
 
 def generate_opacity_file(
@@ -15,17 +26,64 @@ def generate_opacity_file(
     weights: np.ndarray,
     method: str = "grf",
     fits_file: Path | None = None,
+    best_fit_file: Path | None = None,
+    continuum_data: Tuple[np.ndarray, np.ndarray] | None = None,
     do_plot: bool = False,
 ) -> None:
+    """
+    Generate opacity file for silicate grains.
+
+    If a fits file is provided, the continuum opacity is calculated as well.
+
+    Parameters
+    ----------
+    data_dir : Path
+        Directory containing the data.
+    weights : np.ndarray
+        Weights for the different silicate species.
+    method : str, optional
+        Method to use for the opacity calculation. Default is "grf".
+    fits_file : Path, optional
+        Fits file containing the flux data. Default is None.
+    best_fit_file : Path, optional
+        Best fit file containing the best fit parameters. Default is None.
+    continuum_opacity : tuple of np.ndarrays, optional
+        The continuum wavelength and opacity. Default is None.
+    do_plot : bool, optional
+        Whether to plot the opacity. Default is False.
+    """
     wl, opacity = get_opacity(data_dir / "opacities", weights, NAMES, method)
 
     if fits_file is not None:
+        grid_continuum, opacity_continuum = continuum_data
+        labels, params = np.load(best_fit_file)
+        params = params.astype(float)
+
+        characteristic_temperature = params[np.where(labels == "tempc")][0]
+        continuum_weight = params[np.where(labels == "weight_cont")][0] / 1e2
+        scaling_factor = params[np.where(labels == "f")][0]
+
         with fits.open(fits_file) as hdul:
             wl_flux = (hdul["OI_WAVELENGTH"].data["EFF_WAVE"] * u.m).to(u.um).value
-            flux = hdul["OI_FLUX"].data["FLUXDATA"]
-            ind = np.where((wl >= 8.5) & (wl <= wl_flux[-1]))
-            flux = np.interp(wl[ind], wl_flux, flux.flatten())
-            opacity[ind] = flux / flux.max() * opacity[ind].max()
+            ind = np.where((wl >= wl_flux[0]) & (wl <= wl_flux[-1]))[0]
+            flux = hdul["OI_FLUX"].data["FLUXDATA"][0]
+
+        flux_scaling = (
+            10.0**-scaling_factor
+            * BlackBody(characteristic_temperature * u.K)(wl[ind] * u.um)
+            * u.sr
+        ).to(u.Jy)
+        flux = np.interp(wl[ind], wl_flux, flux)
+        continuum_opacity = (
+            np.interp(wl[ind], grid_continuum, opacity_continuum) * continuum_weight
+        )
+        opacity[ind] = (
+            flux - continuum_opacity * flux_scaling.value
+        ) / flux_scaling.value
+
+        lower, upper = (ind[0] - 1, ind[0] + 4), (ind[-1] - 3, ind[-1] + 2)
+        opacity[slice(*lower)] = interpolation_smooth(wl, opacity, lower)
+        opacity[slice(*upper)] = interpolation_smooth(wl, opacity, upper)
 
     if do_plot:
         empirical = "_empirical" if fits_file is not None else ""
@@ -33,8 +91,11 @@ def generate_opacity_file(
         plt.plot(wl, opacity)
         plt.xlabel(r"$\lambda$ ($\mathrm{\mu}$m)")
         plt.ylabel(r"$\kappa$ (cm$^2$ g$^{-1}$)")
-        plt.yscale("log")
-        plt.xlim([0, 30])
+        plt.xlim([7, 15])
+        plt.ylim([None, 3000])
+        if empirical:
+            plt.ylim([None, 1000])
+
         plt.savefig(f"silicate_opacities_{method}{empirical}.pdf", format="pdf")
         plt.close()
 
@@ -52,12 +113,23 @@ if __name__ == "__main__":
 
     method = "grf"
     if method == "boekel":
-        weights = np.array([0, 73.2, 0.6, 14.2, 8.6, 0, 2.4, 1.0, 0, 0]) / 1e2
+        weights = np.array([0, 73.2, 0.6, 14.2, 8.6, 0, 2.4, 1.0, 0, 0])
     else:
-        _, weights = np.load(data_dir / "opacities" / "silicate_labels_and_weights.npy")
-        weights = weights.astype(float) / weights.astype(float).sum()
+        weights = np.load(data_dir / "opacities" / "silicate_labels_and_weights.npy")
 
-    generate_opacity_file(data_dir, weights, method=method, do_plot=True)
-    # generate_opacity_file(
-    #     data_dir, weights, method=method, fits_file=fits_file, do_plot=True
-    # )
+    weights /= 1e2
+    continuum_data = load_data(
+        data_dir / "opacities" / "qval" / "Q_amorph_c_rv0.1.dat",
+        load_func=qval_to_opacity,
+    )
+
+    # generate_opacity_file(data_dir, weights, method=method, do_plot=True)
+    generate_opacity_file(
+        data_dir,
+        weights,
+        method=method,
+        fits_file=fits_file,
+        best_fit_file=data_dir / "opacities" / "best_fit.npy",
+        continuum_data=continuum_data,
+        do_plot=True,
+    )
