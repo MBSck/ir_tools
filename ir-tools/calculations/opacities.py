e904f0aeda531d840b526bfdda737d23f85e987d
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2024-11-07 10:13:39 +0100

    Update opacity calculation

--- ir-tools/calculations/opacities.py
+++ ir-tools/calculations/opacities.py
@@ -21,15 +21,15 @@ def interpolation_smooth(
     )
 
 
-def generate_opacity_file(
+def calculate_opacity(
     data_dir: Path,
     weights: np.ndarray,
     method: str = "grf",
     fits_file: Path | None = None,
     best_fit_file: Path | None = None,
     continuum_data: Tuple[np.ndarray, np.ndarray] | None = None,
-    do_plot: bool = False,
-) -> None:
+    scaling: float = 1.2,
+) -> Tuple[np.ndarray, np.ndarray]:
     """
     Generate opacity file for silicate grains.
 
@@ -49,63 +49,38 @@ def generate_opacity_file(
         Best fit file containing the best fit parameters. Default is None.
     continuum_opacity : tuple of np.ndarrays, optional
         The continuum wavelength and opacity. Default is None.
-    do_plot : bool, optional
-        Whether to plot the opacity. Default is False.
+    scaling : float, optional
+        Scaling factor for the opacity. Default is 1.2.
     """
     wl, opacity = get_opacity(data_dir / "opacities", weights, NAMES, method)
 
     if fits_file is not None:
-        _, opacity_normed = get_opacity(data_dir / "opacities", weights / weights.sum(), NAMES, method)
-        opacity_scaling_factor = (opacity_normed / opacity)[0]
-        grid_continuum, opacity_continuum = continuum_data
+        wl_continuum, opacity_cont = continuum_data
         labels, params = np.load(best_fit_file)
         params = params.astype(float)
 
-        characteristic_temperature = params[np.where(labels == "tempc")][0]
-        continuum_weight = params[np.where(labels == "weight_cont")][0] / 1e2
-        scaling_factor = params[np.where(labels == "f")][0]
+        tempc = params[np.where(labels == "tempc")][0]
+        weight_cont = params[np.where(labels == "weight_cont")][0] / 1e2
+        offset = params[np.where(labels == "f")][0]
 
         with fits.open(fits_file) as hdul:
             wl_flux = (hdul["OI_WAVELENGTH"].data["EFF_WAVE"] * u.m).to(u.um).value
-            ind = np.where((wl >= wl_flux[0]) & (wl <= wl_flux[-1]))[0]
             flux = hdul["OI_FLUX"].data["FLUXDATA"][0]
+            ind = np.where((wl >= wl_flux[0]) & (wl <= wl_flux[-1]))[0]
 
         flux_scaling = (
-            10.0**-scaling_factor
-            * BlackBody(characteristic_temperature * u.K)(wl[ind] * u.um)
-            * u.sr
-        ).to(u.Jy)
-        flux = np.interp(wl[ind], wl_flux, flux)
-        continuum_opacity = (
-            np.interp(wl[ind], grid_continuum, opacity_continuum) * continuum_weight
+            (10.0**-offset * BlackBody(tempc * u.K)(wl[ind] * u.um) * u.sr)
+            .to(u.Jy)
+            .value
         )
-        opacity[ind] = (
-            flux - continuum_opacity * flux_scaling.value
-        ) / flux_scaling.value
+        flux = np.interp(wl[ind], wl_flux, flux)
+        opacity_cont = np.interp(wl[ind], wl_continuum, opacity_cont) * weight_cont
+        opacity[ind] = (flux / flux_scaling - opacity_cont) * scaling
 
         lower, upper = (ind[0] - 1, ind[0] + 4), (ind[-1] - 3, ind[-1] + 2)
         opacity[slice(*lower)] = interpolation_smooth(wl, opacity, lower)
         opacity[slice(*upper)] = interpolation_smooth(wl, opacity, upper)
-        opacity *= opacity_scaling_factor
-
-    if do_plot:
-        empirical = "_empirical" if fits_file is not None else ""
-
-        plt.plot(wl, opacity)
-        plt.xlabel(r"$\lambda$ ($\mathrm{\mu}$m)")
-        plt.ylabel(r"$\kappa$ (cm$^2$ g$^{-1}$)")
-        plt.xlim([7, 15])
-        plt.ylim([None, 3500])
-        if empirical:
-            plt.ylim([None, 3000])
-
-        plt.savefig(f"silicate_opacities_{method}{empirical}.pdf", format="pdf")
-        plt.close()
-
-    np.save(
-        data_dir / "opacities" / f"hd142527_silicate_{method}_opacities.npy",
-        np.array([wl, opacity]),
-    )
+    return wl, opacity
 
 
 if __name__ == "__main__":
@@ -121,18 +96,52 @@ if __name__ == "__main__":
         weights = np.load(data_dir / "opacities" / "silicate_labels_and_weights.npy")
 
     weights /= 1e2
+
     continuum_data = load_data(
         data_dir / "opacities" / "qval" / "Q_amorph_c_rv0.1.dat",
         load_func=qval_to_opacity,
     )
 
-    # generate_opacity_file(data_dir, weights / weights.sum(), method=method, do_plot=True)
-    generate_opacity_file(
+    _, normed_opacity = calculate_opacity(data_dir, weights / weights.sum(), method=method)
+    wl, opacity = calculate_opacity(data_dir, weights, method=method)
+    opacity_scale = max(normed_opacity / opacity)
+
+    fig, ax = plt.subplots()
+    ax.plot(wl, opacity, label="From fit")
+
+    wl, opacity = calculate_opacity(
         data_dir,
         weights,
         method=method,
         fits_file=fits_file,
         best_fit_file=data_dir / "opacities" / "best_fit.npy",
         continuum_data=continuum_data,
-        do_plot=True,
+        scaling=1
+    )
+
+    ax.plot(wl, opacity, label="From data.")
+
+    scaling = 1.2
+    wl, opacity = calculate_opacity(
+        data_dir,
+        weights,
+        method=method,
+        fits_file=fits_file,
+        best_fit_file=data_dir / "opacities" / "best_fit.npy",
+        continuum_data=continuum_data,
+        scaling=1.3
+    )
+
+    ax.plot(wl, opacity, label=f"From data. Scaled by {scaling}x")
+
+    ax.set_xlabel(r"$\lambda$ ($\mathrm{\mu}$m)")
+    ax.set_ylabel(r"$\kappa$ (cm$^2$ g$^{-1}$)")
+    ax.set_xlim([7, 15])
+    ax.set_ylim([None, 1000])
+    ax.legend()
+    plt.savefig("opacity_comparison.pdf", format="pdf")
+
+    np.save(
+        data_dir / "opacities" / f"hd142527_silicate_{method}_opacities.npy",
+        np.array([wl, opacity * opacity_scale]),
     )
--- ir-tools/plots/variability.py
+++ ir-tools/plots/variability.py
@@ -14,16 +14,20 @@ def wise_to_flux(flux: float, lband: bool):
     return np.round(31.674 * 10.0 ** (-flux / 2.5), 2)
 
 
-def query_and_filter_catalogs(object_name: str, radius=1*u.arcsec):
+def query_and_filter_catalogs(object_name: str, radius=1 * u.arcsec):
     """Query Vizier for the specified object and filter the catalogs to
     only include those with WISE data and multiple epochs."""
     Vizier.ROW_LIMIT = -1
     catalogs = Vizier.query_object(object_name, radius=radius)
     multi_epoch_catalogs = []
-    
+
     for catalog in catalogs:
-        has_wise_columns = any(col for col in catalog.colnames if any(f"W{index}" in col for index in range(1, 5)))
-        
+        has_wise_columns = any(
+            col
+            for col in catalog.colnames
+            if any(f"W{index}" in col for index in range(1, 5))
+        )
+
         # has_multiple_epochs(catalog):
         if has_wise_columns:
             multi_epoch_catalogs.append(catalog)
@@ -32,13 +36,13 @@ def query_and_filter_catalogs(object_name: str, radius=1*u.arcsec):
 
 
 # TODO: Check AKARI as well for the targets
-def query_catalog(object_name: str, catalog: str,
-                  index: int, flux: bool = False):
+def query_catalog(object_name: str, catalog: str, index: int, flux: bool = False):
     """Query the specified catalog for the specified object
     from a multi epoch catalog."""
     if catalog == "wise":
-        wise = Irsa.query_region(object_name, catalog="allwise_p3as_mep",
-                                  spatial="Cone", radius=1*u.arcsec)
+        wise = Irsa.query_region(
+            object_name, catalog="allwise_p3as_mep", spatial="Cone", radius=1 * u.arcsec
+        )
 
         w, w_err = wise[f"w{index}mpro_ep"], wise[f"w{index}sigmpro_ep"]
         if flux:
@@ -52,8 +56,10 @@ def query_catalog(object_name: str, catalog: str,
         labels = labels[ind]
         w, w_err = w[ind], w_err[ind]
         isot = [label.isot for label in labels]
-        data = {k: {"time": [], "value": [], "error": []}
-                for k in np.unique([iso.split("T")[0] for iso in isot])}
+        data = {
+            k: {"time": [], "value": [], "error": []}
+            for k in np.unique([iso.split("T")[0] for iso in isot])
+        }
 
         for w_entry, w_err_entry, iso in zip(w, w_err, isot):
             key, time = iso.split("T")
@@ -63,25 +69,36 @@ def query_catalog(object_name: str, catalog: str,
     return {k: {ki: np.array(vi) for ki, vi in v.items()} for k, v in data.items()}
 
 
-def plot_multi_epoch(object_name: str, flux: bool = False,
-                     flux_ranges: List[List[float]] = [[None, None], [None, None]]) -> None:
+def plot_multi_epoch(
+    object_name: str,
+    flux: bool = False,
+    flux_ranges: List[List[float]] = [[None, None], [None, None]],
+) -> None:
     """Plot the multi-epoch WISE data for the specified object."""
     # akari_data = query_catalog(object_name, "akari")
 
-    _, axarr = plt.subplots(1, 2, figsize=(15, 7))
-    for index, (wise_index, ax) in enumerate(zip([1, 3], axarr.flatten())):
-        wise_data = query_catalog(object_name, "wise", wise_index, flux)
-        errorbar_kwargs = {"fmt": "o", "ecolor": "gray", "capsize": 5}
-        for key, value in wise_data.items():
-            ax.errorbar(value["time"], value["value"], value["error"],
-                        label=key, **errorbar_kwargs)
-        ax.set_ylabel("Luminosity (mag)" if not flux else "Flux (Jy)")
-        ax.set_xlabel("Time (UTC)")
+    # NOTE: The WISE bands to be plotted
+    wise_bands = [1, 2, 3, 4]
+    errorbar_kwargs = {"fmt": "o", "ecolor": "gray", "capsize": 5}
+    wise_data = [
+        query_catalog(object_name, "wise", index, flux) for index in wise_bands
+    ]
+
+    for index, data in enumerate(wise_data):
+        plt.errorbar(
+            data["time"],
+            data["value"],
+            data["error"],
+            label=f"WISE{index}",
+            **errorbar_kwargs,
+        )
         if flux:
-            ax.set_ylim(flux_ranges[index])
-        ax.set_title(f"{object_name.upper()} WISE{wise_index}")
-        ax.tick_params(axis="x", labelsize=6, rotation=45)
-        ax.legend()
+            plt.ylim(flux_ranges[index])
+
+    plt.tick_params(axis="x", labelsize=6, rotation=45)
+    plt.ylabel("Luminosity (mag)" if not flux else "Flux (Jy)")
+    plt.xlabel("Time (UTC)")
+    plt.legend()
 
     plt.savefig(f"{object_name}_time_variability.pdf", format="pdf")
 
@@ -97,7 +114,7 @@ if __name__ == "__main__":
     # catalog_list = Vizier.find_catalogs("WISE")
     # breakpoint()
     # pprint({k:v.description for k,v in catalog_list.items()})
-    
+
     # catalogs = Irsa.list_catalogs()
     plot_multi_epoch("hd142527", False)
     # plot_multi_epoch("hd142527", True, flux_ranges=[[4, None], [8, None]])
