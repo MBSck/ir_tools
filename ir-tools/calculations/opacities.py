Fix opacity calculation

ir-tools/calculations/opacities.py
@@ -5,10 +5,61 @@ import astropy.units as u
 import matplotlib.pyplot as plt
 import numpy as np
 from astropy.io import fits
-from ppdmod.utils import get_opacity, load_data, qval_to_opacity
 from astropy.modeling.models import BlackBody
+from ppdmod.utils import get_opacity, load_data, qval_to_opacity
 
 NAMES = ["Pyroxene", "Enstatite", "Forsterite", "Silica", "Olivine"]
+
+
+def plot_sed(
+    data_dir: Path,
+    source_dir: Path,
+    flux_data: Tuple[np.ndarray, np.ndarray],
+    continuum_data: Tuple[np.ndarray, np.ndarray],
+    savefig: str | None = None,
+    method: str = "grf",
+) -> None:
+    labels, params = np.load(source_dir / "best_sed_fit.npy")
+    params = params.astype(float)
+
+    tempc = params[np.where(labels == "tempc")][0]
+    weight_cont = params[np.where(labels == "weight_cont")][0] / 1e2
+    offset = params[np.where(labels == "f")][0]
+    indices = list(
+        map(
+            labels.tolist().index,
+            filter(lambda x: "weight" in x and "cont" not in x, labels),
+        )
+    )
+
+    weights = params[indices] / 1e2
+    wl_cont, op_cont = continuum_data
+    wl_sil, op_sil = get_opacity(data_dir / "opacities", weights, NAMES, method)
+    op_sil = np.interp(wl_cont, wl_sil, op_sil)
+
+    wl_flux, flux = flux_data
+    bb = (BlackBody(tempc * u.K)(wl_cont * u.um) * 10.0**-offset * u.sr).to(u.Jy)
+    flux_cont = bb * op_cont * weight_cont
+    flux_model = bb * (op_sil + op_cont * weight_cont)
+
+    _, ax = plt.subplots()
+    ax.plot(wl_flux, flux, label="Data")
+    ax.plot(wl_cont, flux_model, label="Model")
+    ax.plot(wl_cont, flux_cont, label="Continuum")
+    ax.set_xlabel(r"$\lambda$ ($\mathrm{\mu}$m)")
+    ax.set_ylabel(r"$F_{\nu}$ (Jy)")
+    ax.set_xscale("log")
+    ax.set_yscale("log")
+    ax.set_xlim([0.1, 100])
+    ax.set_ylim([0.1, 100])
+    ax.legend()
+
+    if savefig is not None:
+        plt.savefig(savefig, format="pdf")
+    else:
+        plt.show()
+
+    plt.close()
 
 
 def interpolation_smooth(
@@ -25,10 +76,9 @@ def calculate_opacity(
     data_dir: Path,
     weights: np.ndarray,
     method: str = "grf",
-    fits_file: Path | None = None,
     best_fit_file: Path | None = None,
     continuum_data: Tuple[np.ndarray, np.ndarray] | None = None,
-    scaling: float = 1.2,
+    flux_data: Tuple[np.ndarray, np.ndarray] | None = None,
 ) -> Tuple[np.ndarray, np.ndarray]:
     """
     Generate opacity file for silicate grains.
@@ -43,19 +93,20 @@ def calculate_opacity(
         Weights for the different silicate species.
     method : str, optional
         Method to use for the opacity calculation. Default is "grf".
-    fits_file : Path, optional
-        Fits file containing the flux data. Default is None.
     best_fit_file : Path, optional
         Best fit file containing the best fit parameters. Default is None.
-    continuum_opacity : tuple of np.ndarrays, optional
+    continuum_data : tuple of np.ndarrays, optional
         The continuum wavelength and opacity. Default is None.
-    scaling : float, optional
-        Scaling factor for the opacity. Default is 1.2.
+    flux_data : Path, optional
+        The flux wavelength and flux data. Default is None.
     """
-    wl, opacity = get_opacity(data_dir / "opacities", weights, NAMES, method)
+    wl, op_sil = get_opacity(data_dir / "opacities", weights, NAMES, method)
 
-    if fits_file is not None:
-        wl_continuum, opacity_cont = continuum_data
+    if flux_data is not None:
+        wl_cont, op_cont = continuum_data
+        wl_flux, flux = flux_data
+        ind = np.where((wl >= wl_flux[0]) & (wl <= wl_flux[-1]))[0]
+
         labels, params = np.load(best_fit_file)
         params = params.astype(float)
 
@@ -63,37 +114,37 @@ def calculate_opacity(
         weight_cont = params[np.where(labels == "weight_cont")][0] / 1e2
         offset = params[np.where(labels == "f")][0]
 
-        with fits.open(fits_file) as hdul:
-            wl_flux = (hdul["OI_WAVELENGTH"].data["EFF_WAVE"] * u.m).to(u.um).value
-            flux = hdul["OI_FLUX"].data["FLUXDATA"][0]
-            ind = np.where((wl >= wl_flux[0]) & (wl <= wl_flux[-1]))[0]
-
         flux_scaling = (
             (10.0**-offset * BlackBody(tempc * u.K)(wl[ind] * u.um) * u.sr)
             .to(u.Jy)
             .value
         )
         flux = np.interp(wl[ind], wl_flux, flux)
-        opacity_cont = np.interp(wl[ind], wl_continuum, opacity_cont) * weight_cont
-        opacity[ind] = (flux / flux_scaling - opacity_cont) * scaling
+        op_cont = np.interp(wl[ind], wl_cont, op_cont) * weight_cont
+        op_sil[ind] = (flux / flux_scaling - op_cont)
 
-        lower, upper = (ind[0] - 1, ind[0] + 4), (ind[-1] - 3, ind[-1] + 2)
-        opacity[slice(*lower)] = interpolation_smooth(wl, opacity, lower)
-        opacity[slice(*upper)] = interpolation_smooth(wl, opacity, upper)
-    return wl, opacity
+    return wl, op_sil
 
 
+# TODO: Fix the calculation of the opacity at its edges
 if __name__ == "__main__":
     data_dir = Path().home() / "Data"
+    plot_dir = Path("plots")
+    source_dir = data_dir / "model_results" / "hd142527"
     fits_file = list(
         (data_dir / "fitting_data" / "hd142527" / "sed_fit" / "averaged").glob("*")
     )[0]
+
+    with fits.open(fits_file) as hdul:
+        wl_flux = (hdul["OI_WAVELENGTH"].data["EFF_WAVE"] * u.m).to(u.um).value
+        flux = hdul["OI_FLUX"].data["FLUXDATA"][0]
+        flux_data = wl_flux, flux
 
     method = "grf"
     if method == "boekel":
         weights = np.array([0, 73.2, 0.6, 14.2, 8.6, 0, 2.4, 1.0, 0, 0])
     else:
-        weights = np.load(data_dir / "opacities" / "silicate_labels_and_weights.npy")
+        weights = np.load(source_dir / f"silicate_weights_{method}.npy")
 
     weights /= 1e2
 
@@ -102,51 +153,42 @@ if __name__ == "__main__":
         load_func=qval_to_opacity,
     )
 
-    _, normed_opacity = calculate_opacity(data_dir, weights / weights.sum(), method=method)
-    wl, opacity = calculate_opacity(data_dir, weights, method=method)
-    opacity_scale = max(normed_opacity / opacity)
+    plot_sed(
+        data_dir,
+        source_dir,
+        flux_data,
+        continuum_data,
+        method=method,
+        savefig=plot_dir / "nband_fit.pdf",
+    )
 
-    fig, ax = plt.subplots()
-    ax.plot(wl, opacity, label="From fit")
+    _, op_sil_norm = calculate_opacity(
+        data_dir, weights / weights.sum(), method=method
+    )
+    _, op_sil = calculate_opacity(data_dir, weights, method=method)
+    opacity_scale = max(op_sil_norm / op_sil)
 
-    wl, opacity_data = calculate_opacity(
+    wl, op_new_sil = calculate_opacity(
         data_dir,
         weights,
         method=method,
-        fits_file=fits_file,
-        best_fit_file=data_dir / "opacities" / "best_fit.npy",
+        best_fit_file=source_dir / "best_sed_fit.npy",
+        flux_data=flux_data,
         continuum_data=continuum_data,
-        scaling=1
     )
 
-    ax.plot(wl, opacity_data, label="From data.")
-
-    scaling = 1.2
-    wl, opacity_data_scaled = calculate_opacity(
-        data_dir,
-        weights,
-        method=method,
-        fits_file=fits_file,
-        best_fit_file=data_dir / "opacities" / "best_fit.npy",
-        continuum_data=continuum_data,
-        scaling=1.3
-    )
-
-    ax.plot(wl, opacity_data_scaled, label=f"From data. Scaled by {scaling}x")
-
+    fig1, ax = plt.subplots()
+    ax.plot(wl, op_sil * opacity_scale, label="From fit")
+    ax.plot(wl, op_new_sil * opacity_scale, label="From data.")
     ax.set_xlabel(r"$\lambda$ ($\mathrm{\mu}$m)")
     ax.set_ylabel(r"$\kappa$ (cm$^2$ g$^{-1}$)")
-    ax.set_xlim([7, 15])
-    ax.set_ylim([None, 1000])
+    ax.set_xlim([7.5, 13.15])
+    ax.set_ylim([0, 3000])
     ax.legend()
-    plt.savefig("opacity_comparison.pdf", format="pdf")
+    plt.savefig(plot_dir / "op_sil_comparison.pdf", format="pdf")
 
-    txt_file = data_dir / "opacities" / f"hd142527_silicate_{method}_opacities.txt"
-
-    labels, params = np.load(data_dir / "opacities" / "best_fit.npy")
-    comments = "\n".join(labels + ":\t\t" + params)
-    np.savetxt(txt_file, np.column_stack([wl, opacity_data_scaled]), header=comments)
+    labels, params = np.load(source_dir / "best_sed_fit.npy")
     np.save(
-        data_dir / "opacities" / f"hd142527_silicate_{method}_opacities.npy",
-        np.array([wl, opacity * opacity_scale]),
+        source_dir / f"hd142527_silicate_{method}_opacities.npy",
+        np.array([wl, op_sil * opacity_scale]),
     )