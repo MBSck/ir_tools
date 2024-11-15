Correct opacity calculation

ir-tools/calculations/opacities.py
@@ -190,5 +190,5 @@ if __name__ == "__main__":
     labels, params = np.load(source_dir / "best_sed_fit.npy")
     np.save(
         source_dir / f"silicate_{method}_opacities.npy",
-        np.array([wl, op_sil * opacity_scale]),
+        np.array([wl, op_new_sil * opacity_scale]),
     )