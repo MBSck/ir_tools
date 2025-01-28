290b2a0d8641288d1c8c226bf0d0f3be94e40947
Marten B. Scheuck <martenscheuck@gmail.com>
2025-01-28 15:26:09 +0100

    Remove script

--- ir-tools/fit_results/disc.py
+++ ir-tools/fit_results/disc.py
@@ -4,11 +4,9 @@ from pathlib import Path
 import astropy.units as u
 import numpy as np
 from dynesty import DynamicNestedSampler
-from matadrs.utils.plot import Plotter
 from ppdmod.data import set_data
 from ppdmod.fitting import (
     compute_interferometric_chi_sq,
-    compute_observables,
     get_labels,
     get_theta,
     get_units,
@@ -24,45 +22,14 @@ from ppdmod.plot import (
     plot_intermediate_products,
     plot_overview,
 )
-from ppdmod.utils import windowed_linspace
+from ppdmod.utils import (
+    create_adaptive_bins,
+    windowed_linspace,
+)
 
 from ..tables import best_fit_parameters
 
 np.seterr(over="ignore", divide="ignore")
-
-# plot_kwargs = dict(
-#     legend_format="short",
-#     error=True,
-#     subplots=True,
-#     margin=0.3,
-#     legend_size="medium",
-#     sharex=True,
-#     share_legend=True,
-#     save=True,
-# )
-
-# lband_data = list(fits_dir.glob("*HAW*"))
-# plot_fits = Plotter(lband_data[:3], plot_name="lband_data.pdf", save_dir=plot_dir)
-# plot_fits.add_uv(uv_extent=150).add_flux().add_vis(corr_flux=True).add_t3(
-#     unwrap=False
-# )
-# plot_fits.plot(**plot_kwargs)
-#
-# plot_fits = Plotter(
-#     lband_data[4:], plot_name="lband_data_continuation.pdf", save_dir=plot_dir
-# )
-# plot_fits.add_uv(uv_extent=150).add_flux().add_vis(corr_flux=True).add_t3(
-#     unwrap=False
-# )
-# plot_fits.plot(**plot_kwargs)
-#
-# plot_fits = Plotter(
-#     list(fits_dir.glob("*AQU*")), plot_name="nband_data.pdf", save_dir=plot_dir
-# )
-# plot_fits.add_uv(uv_extent=150).add_flux().add_vis(corr_flux=True).add_t3(
-#     unwrap=True
-# )
-# plot_fits.plot(**plot_kwargs)
 
 
 def ptform():
@@ -72,44 +39,33 @@ def ptform():
 # TODO: Fix the chi square here to get correct fit values
 if __name__ == "__main__":
     data_dir = Path().home() / "Data"
-    path = data_dir / "model_results" / "disc_fits" / "2024-12-03"
-    path /= "test_average_all_free"
+    path = data_dir / "results" / "disc" / "2025-01-28"
+    path /= "no_reduced_t3"
     plot_dir, assets_dir = path / "plots", path / "assets"
     plot_dir.mkdir(exist_ok=True, parents=True)
     assets_dir.mkdir(exist_ok=True, parents=True)
 
-    fits_dir = data_dir / "fitting_data" / "hd142527"
-    binning = OPTIONS.data.binning
+    fits_dir = data_dir / "fitting" / "hd142527"
+    nband_wavelengths, nband_binning_windows = create_adaptive_bins(
+        [8, 13], [9.2, 11.9], 0.2, 0.65
+    )
     wavelengths = {
         "hband": [1.7] * u.um,
         "kband": [2.15] * u.um,
-        "lband": windowed_linspace(3.1, 3.4, binning.lband.value) * u.um,
-        "mband": windowed_linspace(4.7, 4.9, binning.mband.value) * u.um,
-        "nband": windowed_linspace(8, 13, binning.nband.value) * u.um,
+        "lband": windowed_linspace(3.1, 3.4, OPTIONS.data.binning.lband.value) * u.um,
+        "mband": windowed_linspace(4.7, 4.9, OPTIONS.data.binning.mband.value) * u.um,
+        "nband": nband_wavelengths * u.um,
     }
-
-    OPTIONS.model.output = "non-normed"
+    OPTIONS.data.binning.nband = nband_binning_windows * u.um
     fits_files = list((fits_dir).glob("*fits"))
 
     dim = 1024
-    wavelengths = np.concatenate(
-        (
-            wavelengths["hband"],
-            wavelengths["kband"],
-            wavelengths["lband"],
-            wavelengths["mband"],
-            wavelengths["nband"],
-        )
-    )
+    bands = ["hband", "kband", "lband", "mband", "nband"]
+    wavelengths = np.concatenate([wavelengths[band] for band in bands])
+    fit_data = ["flux", "vis", "t3"]
+    # fit_data = ["flux", "vis"]
     data = set_data(
-        fits_files,
-        wavelengths=wavelengths,
-        # fit_data=["flux", "vis"],
-        fit_data=["flux", "vis", "t3"],
-        # fit_data=["t3"],
-        # set_std_err=["mband"],
-        # weights=[1.0, 0.02094934],
-        average=True
+        fits_files, wavelengths=wavelengths, fit_data=fit_data, average=True
     )
     uncertainties = np.load(path / "uncertainties.npy")
     with open(path / "components.pkl", "rb") as f:
@@ -118,9 +74,9 @@ if __name__ == "__main__":
     theta = get_theta(components)
     component_labels = [component.label for component in components]
 
-    # TODO: Check why the chi_sq is different here from the value that it should have
+    # TODO: Check why the chi_sq is different here from the value that it should be?
     rchi_sqs = compute_interferometric_chi_sq(
-        *compute_observables(components),
+        components,
         ndim=np.array(theta).size,
         method="linear",
         reduced=True,
@@ -131,14 +87,17 @@ if __name__ == "__main__":
     labels = get_labels(components)
     units = get_units(components)
     sampler = DynamicNestedSampler.restore(path / "sampler.save")
-    plot_corner(sampler, labels, units, savefig=plot_dir / "corner.pdf")
-    # plot_chains(sampler, labels, units, savefig=plot_dir / "chains.pdf")
 
-    plot_overview(savefig=plot_dir / "overview.pdf")
-    plot_overview(bands=["nband"], savefig=plot_dir / "overview_nband.pdf")
+    plot_format = "pdf"
+    plot_corner(sampler, labels, units, savefig=(plot_dir / f"corner.{plot_format}"))
+    plot_overview(savefig=(plot_dir / f"overview.{plot_format}"))
+    plot_overview(
+        bands=["nband"],
+        savefig=(plot_dir / f"overview_nband.{plot_format}"),
+    )
     plot_overview(
         bands=["hband", "kband", "lband", "mband"],
-        savefig=plot_dir / "overview_hlkmband.pdf",
+        savefig=(plot_dir / f"overview_hlkmband.{plot_format}"),
     )
     best_fit_parameters(
         labels,
@@ -156,22 +115,40 @@ if __name__ == "__main__":
         save_as_csv=False,
         savefig=assets_dir / "disc",
     )
-    plot_fit(components=components, savefig=plot_dir / "disc.pdf")
+    plot_fit(components=components, savefig=(plot_dir / f"disc.{plot_format}"))
     plot_fit(
-        components=components, bands=["nband"], savefig=plot_dir / "disc_nband.pdf"
+        components=components,
+        bands=["nband"],
+        savefig=(plot_dir / f"disc_nband.{plot_format}"),
     )
     plot_fit(
         components=components,
         bands=["hband", "kband", "lband", "mband"],
         ylims={"t3": [-15, 15]},
-        savefig=plot_dir / "disc_hklmband.pdf",
+        savefig=(plot_dir / f"disc_hklmband.{plot_format}"),
     )
     zoom = 5
     plot_components(
-        components, dim, 0.1, 3.5, norm=0.2, zoom=zoom, savefig=plot_dir / "image_lband.png"
+        components,
+        dim,
+        0.1,
+        3.5,
+        norm=0.1,
+        zoom=zoom,
+        savefig=plot_dir / "image_lband.png",
+    )
+
+    OPTIONS.data.binning.nband = (
+        np.interp(10.5, nband_wavelengths, nband_binning_windows) * u.um
     )
     plot_components(
-        components, dim, 0.1, 10.5, norm=0.2, zoom=zoom, savefig=plot_dir / "image_nband.png"
+        components,
+        dim,
+        0.1,
+        10.5,
+        norm=0.1,
+        zoom=zoom,
+        savefig=plot_dir / "image_nband.png",
     )
     # plot_component_mosaic(
     #     components, dim, 0.1, norm=0.2, savefig=plot_dir / "models.pdf", zoom=8
