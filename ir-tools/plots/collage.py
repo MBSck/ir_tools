502e3fe9ff3b115a2dd0d268cb63d1c1b98c268a
Marten B. Scheuck <martenscheuck@gmail.com>
2025-02-04 23:41:59 +0100

    Make uv plotting work

--- ir-tools/plots/collage.py
+++ ir-tools/plots/collage.py
@@ -4,15 +4,19 @@ from pathlib import Path
 from typing import List
 
 import astropy.units as u
+import matplotlib.lines as mlines
 import matplotlib.pyplot as plt
 import numpy as np
 from astropy.io import fits
+from matplotlib import colormaps as mcm
 from matplotlib.axes import Axes
+from matplotlib.colors import ListedColormap
 from tqdm import tqdm
 
 
 @dataclass
 class Data:
+    nentry: int = 0
     header: List[fits.Header] = field(default_factory=list)
     wl: List[fits.BinTableHDU] = field(default_factory=list)
     array: List[fits.BinTableHDU] = field(default_factory=list)
@@ -25,17 +29,18 @@ def read_data(fits_files: List[Path] | Path) -> Data:
     """Reads the data from the fits files."""
     data = Data()
     for fits_file in fits_files if isinstance(fits_files, list) else [fits_files]:
+        data.nentry += 1
         index = 20 if "GRAV" in fits_file.stem else None
         with fits.open(fits_file) as hdul:
             for key in ["header", "wl", "array", "vis", "vis2", "t3"]:
                 if key == "header":
-                    data.header.append(hdul[0].header)
+                    data.header.append(hdul[0].header.copy())
                 elif key == "wl":
                     data.wl.append(
-                        (hdul["oi_wavelength", index].data["eff_wave"] * u.m).to(u.um)
+                        (hdul["oi_wavelength", index].data["eff_wave"].copy() * u.m).to(u.um)
                     )
                 else:
-                    getattr(data, key).append(hdul[f"oi_{key}", index])
+                    getattr(data, key).append(hdul[f"oi_{key}", index].copy())
     return data
 
 
@@ -45,38 +50,76 @@ def get_unit(self, header: str, sub_header: str) -> str:
     return str(unit) if unit is not None else "a.u."
 
 
-def plot_uv(ax: Axes, data: Data, index: int) -> Axes:
+def convert_style_to_colormap(style: str) -> ListedColormap:
+    """Converts a style into a colormap."""
+    plt.style.use(style)
+    colormap = ListedColormap(
+            plt.rcParams["axes.prop_cycle"].by_key()["color"])
+    plt.style.use("default")
+    return colormap
+
+
+def get_colormap(colormap: str) -> ListedColormap:
+    """Gets the colormap as the matplotlib colormaps or styles."""
+    try:
+        return mcm.get_cmap(colormap)
+    except ValueError:
+        return convert_style_to_colormap(colormap)
+
+
+def get_colorlist(colormap: str, ncolors: int | None) -> List[str]:
+    """Gets the colormap as a list from the matplotlib colormaps."""
+    return [get_colormap(colormap)(i) for i in range(ncolors)]
+
+
+def plot_uv(ax: Axes, data: Data, index: int | None = None) -> Axes:
     """Plots the uv coverage."""
-    ucoords, vcoords = data.vis2.data["ucoord"], data.vis2.data["vcoord"]
-    # if color_by == "file":
-    #     color = colors[index]
-    #     handles.append(mlines.Line2D(
-    #         [], [], color=color, marker="X",
-    #         linestyle="None", label=readout.date[:-8]))
-    # elif color_by == "instrument":
-    #     if readout.instrument not in instruments:
-    #         instruments.append(readout.instrument)
-    #     color = colors[instruments.index(readout.instrument)]
+    handles = []
+    colors = get_colorlist("tab20", data.nentry)
+    if index is not None:
+        ucoords = data.vis2[index].data["ucoord"]
+        vcoords = data.vis2[index].data["vcoord"]
+        color = colors[index]
+        ax.plot(ucoords, vcoords, "x", markersize=6, markeredgewidth=2, color=color)
+        ax.plot(-ucoords, -vcoords, "x", markersize=6, markeredgewidth=2, color=color)
+    else:
+        ucoords, vcoords = [], []
+        for file_idx, (header, vis2) in enumerate(zip(data.header, data.vis2)):
+            color = colors[file_idx]
+            file_ucoords = vis2.data["ucoord"]
+            file_vcoords = vis2.data["vcoord"]
+            ucoords.extend(file_ucoords)
+            vcoords.extend(file_vcoords)
 
-    for uv_index, (ucoord, vcoord) in enumerate(zip(ucoords, vcoords)):
-        ax.plot(ucoord, vcoord, "x", color=color, markersize=10, markeredgewidth=3)
-        ax.plot(-ucoord, -vcoord, "x", color=color, markersize=10, markeredgewidth=3)
+            # Plot all points for this file with the same color
+            ax.plot(file_ucoords, file_vcoords, "x", markersize=6, markeredgewidth=2, color=color)
+            ax.plot(-file_ucoords, -file_vcoords, "x", markersize=6, markeredgewidth=2, color=color)
 
-        # if show_text:
-        #     ax.text(
-        #         -ucoord - 3.5,
-        #         -vcoord - 1.5,
-        #         uv_index
-        #         fontsize="small",
-        #         color="0",
-        #         alpha=0.8,
-        #     )
+            # Add a legend handle for this file
+            handles.append(mlines.Line2D(
+                [], [], color=color, marker="X", linestyle="None",
+                label=header["date-obs"].split("T")[0]
+            ))
+
+        ucoords, vcoords = np.array(ucoords), np.array(vcoords)
+
+    sorted_idx = np.argsort(np.hypot(ucoords, vcoords))
+    coord_to_count = dict(zip(ucoords[sorted_idx], np.arange(1, sorted_idx.size + 1)))
+    for ucoord, vcoord in zip(ucoords, vcoords):
+        ax.text(
+            ucoord + 3.5,
+            vcoord + 3.5,
+            coord_to_count[ucoord],
+            fontsize="small",
+            color="0",
+            alpha=0.8,
+        )
 
     ax.plot([0.0], [0.0], "+k", markersize=5, markeredgewidth=2, alpha=0.5)
-    # TODO: Implement check or calculation for the orientations
     xlabel, ylabel = "$u$ (m) - South", "$v$ (m) - East"
-    # uv_extent = int(uv_max + uv_max*0.25) if uv_extent is None else uv_extent
+    ax.legend(handles=handles, fontsize="small")
 
+    plt.gca().invert_xaxis()
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
 
@@ -93,7 +136,9 @@ def plot_data(
     return ax
 
 
-def plot_flux(ax: Axes, hdul, x: np.ndarray, index: int) -> Axes:
+# TODO: Finish this here
+# TODO: Move all this here to paper
+def plot_flux(ax: Axes, data: Data, index: int | None = None) -> Axes:
     """Plots the flux."""
     card = hdul["oi_flux", index]
     key = "fluxdata" if "FLUXDATA" in card.columns.names else "flux"
@@ -103,7 +148,7 @@ def plot_flux(ax: Axes, hdul, x: np.ndarray, index: int) -> Axes:
     return ax
 
 
-def plot_vis(ax: Axes, data: Data, index: int) -> Axes:
+def plot_vis(ax: Axes, data: Data, index: int | None = None) -> Axes:
     """Plots the visibility or the correlated flux."""
     x, dataset = data.wl[index], data.vis[index].data
     plot_data(ax, x, dataset["visamp"], dataset["visamperr"], dataset["flag"])
@@ -112,7 +157,7 @@ def plot_vis(ax: Axes, data: Data, index: int) -> Axes:
     return ax
 
 
-def plot_visphi(ax: Axes, data: Data, index: int) -> Axes:
+def plot_visphi(ax: Axes, data: Data, index: int | None = None) -> Axes:
     """Plots the differential phases."""
     x, dataset = data.wl[index], data.vis[index].data
     plot_data(ax, x, dataset["visphi"], dataset["visphierr"], dataset["flag"])
@@ -121,7 +166,7 @@ def plot_visphi(ax: Axes, data: Data, index: int) -> Axes:
     return ax
 
 
-def plot_vis2(ax: Axes, data: Data, index: int) -> Axes:
+def plot_vis2(ax: Axes, data: Data, index: int | None = None) -> Axes:
     """Plots the squared visibility."""
     x, dataset = data.wl[index], data.vis2[index].data
     plot_data(ax, x, dataset["vis2data"], dataset["vis2err"], dataset["flag"])
@@ -131,7 +176,7 @@ def plot_vis2(ax: Axes, data: Data, index: int) -> Axes:
     return ax
 
 
-def plot_t3(ax: Axes, data: Data, index: int) -> Axes:
+def plot_t3(ax: Axes, data: Data, index: int | None = None) -> Axes:
     """Plots the closure phases."""
     x, dataset = data.wl[index], data.t3[index].data
     plot_data(ax, x, dataset["t3phi"], dataset["t3phierr"], dataset["flag"])
@@ -159,25 +204,33 @@ def plot(
     if plots == "all":
         plots = ["uv", "flux", "t3", "vis2", "vis", "visphi"]
 
-    cols, rows = len(plots), int(np.ceil(len(data.vis) / len(plots)))
-    figsize = (cols * cell_width, rows * cell_width)
-    _, axarr = plt.subplots(
-        rows, cols, figsize=figsize, sharex=True, constrained_layout=True
-    )
-
     if kind == "collage":
+        cols, rows = len(plots), int(np.ceil(len(data.vis) / len(plots)))
+        figsize = (cols * cell_width, rows * cell_width)
+        _, axarr = plt.subplots(
+            rows, cols, figsize=figsize, sharex=True, constrained_layout=True
+        )
         for row_index, ax_row in enumerate(axarr):
-            for plot, ax_col in zip(plots, ax_row):
+            for ax_col, plot in zip(ax_row, plots):
                 getattr(module, f"plot_{plot}")(ax_col, data, row_index)
+
     elif kind == "combined":
-        ...
+        cols, rows = len(plots), 1
+        figsize = (cols * cell_width, rows * cell_width)
+        _, axarr = plt.subplots(
+            rows, cols, figsize=figsize, sharex=True, constrained_layout=True
+        )
+        axarr = [axarr] if not isinstance(axarr, np.ndarray) else axarr
+        for ax, plot in zip(axarr, plots):
+            getattr(module, f"plot_{plot}")(ax, data)
+
     # TODO: Implement the individual plots again
     else:
         ...
 
     # TODO: Reimplement different plots from this (as in saved in different files)
     if save_dir is not None:
-        plt.savefig(save_dir, format="png", dpi=300)
+        plt.savefig(save_dir, format=save_dir.suffix[1:], dpi=300)
     else:
         plt.show()
 
@@ -188,9 +241,13 @@ if __name__ == "__main__":
     path = Path().home() / "Data" / "fitting" / "hd142527"
     plot_dir = path / "plots"
     plot_dir.mkdir(parents=True, exist_ok=True)
+
+    fits_files = list(path.glob("*_N_*"))
+    fits_files = [file for file in fits_files if not file.name.startswith("._")]
     plot(
-        list(path.glob("*_N_*.fits")),
+        fits_files,
         # ["flux", "t3", "vis2", "vis"],
         ["uv"],
-        save_dir=plot_dir,
+        kind="combined",
+        save_dir=plot_dir / "uts_uv.png",
     )
