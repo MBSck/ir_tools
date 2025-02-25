6ba6e2f663701b80498753b92898a64fe04aa5b1
M. B. Scheuck <programming.mbsck@mailbox.org>
2025-02-25 13:43:21 +0100

    Test some OBs

--- ir_tools/observations/make_obs.py
+++ ir_tools/observations/make_obs.py
@@ -1,8 +1,15 @@
 from pathlib import Path
 
-from p2obt import create_obs
+from p2obt import create_obs, create_ob
 
 if __name__ == "__main__":
-    data_dir = Path().home() / "Data"
-    night_plan_dir = data_dir / "observations" / "P115"
-    create_obs(night_plan_dir / "image.txt", user_name="MbS")
+    # data_dir = Path().home() / "Data"
+    # night_plan_dir = data_dir / "observations" / "P115"
+    # create_obs(night_plan_dir / "image.txt", user_name="MbS")
+    create_ob(
+        "M8E-IR",
+        "sci",
+        "extended",
+        mode="st",
+        output_dir=Path().cwd(),
+    )
