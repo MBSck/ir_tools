8bb8d2c4eb5c764597ac506e67fc59e9a6baa60a
M. B. Scheuck <programming.mbsck@mailbox.org>
2025-02-27 21:47:08 +0100

    Start work on flux averaging

--- ir_tools/data_prep/utils.py
+++ ir_tools/data_prep/utils.py
@@ -1,35 +1,64 @@
-import shutil
 from pathlib import Path
 from typing import List
 
 import numpy as np
-from astropy.io import fits
-from uncertainties import unumpy
+from astropy.stats import biweight_location
 
 
-# TODO: Exchange this all here with the io library that I am writing
-def average_flux(fits_files: List[Path]):
-    new_fits = None
-    wavelengths, fluxes, errors = [], [], []
+# TODO: For io use my io script
+def average_flux(
+    fits_files: Path | List[Path],
+    method: str = "biweight",
+    save_dir: Path | None = None,
+) -> None:
+    """Averages the "oi_flux" extension of a single fits_file or a list of fits files.
+
+    Parameters
+    ----------
+    fits_files : pathlib.Path or list of pathlib.Path
+    method : str, optional
+    save_dir : pathlib.Path, optional
+
+    Notes
+    -----
+    If a single file is given, it will average all the fluxes in it to return one
+    """
+    if not isinstance(fits_files, (list, tuple, np.ndarray)):
+        fits_files = [fits_files]
+
+    if method == "biweight":
+        avgfunc = biweight_location
+    elif method == "mean":
+        avgfunc = np.nanmean
+    else:
+        avgfunc = np.nanmedian
+
+    flux, flux_err = [], []
     for fits_file in fits_files:
-        if new_fits is None:
-            new_fits = "averaged_flux.fits"
-            shutil.copy(fits_file, new_fits)
-
         with fits.open(fits_file) as hdul:
-            wavelengths.append(hdul["oi_wavelength"].data["eff_wave"].flatten())
-            fluxes.append(hdul["oi_flux"].data["fluxdata"].flatten())
-            errors.append(hdul["oi_flux"].data["fluxerr"].flatten())
+            flux.append(hdul["oi_flux"].data["fluxdata"])
+            flux_err.append(hdul["oi_flux"].data["fluxerr"])
 
-    wavelengths = np.array(wavelengths)
-    fluxes, errors = np.array(fluxes), np.array(errors)
-    fluxes[0] = np.interp(wavelengths[1], wavelengths[0], fluxes[0])
-    fluxes = unumpy.umatrix(fluxes, errors)
-    wavelengths, fluxes = wavelengths[1], np.nanmean(fluxes, axis=0)
-    fluxes, errors = unumpy.nominal_values(fluxes), unumpy.std_devs(fluxes)
+    flux, flux_err = np.array(flux), np.array(flux_err)
 
-    with fits.open(new_fits, "update") as hdul:
-        hdul["oi_wavelength"].data["eff_wave"] = wavelengths
-        hdul["oi_flux"].data["fluxdata"] = fluxes
-        hdul["oi_flux"].data["fluxerr"] = errors
-        hdul.flush()
+    if len(fits_files) > 1:
+        avg_flux = avgfunc(flux, axis=0)
+    else:
+        avg_flux = avgfunc(flux)
+
+    # TODO: Make sure the error calculation here is correct
+    # if len(flux_arr) > 3:
+    #     avg_fluxerr = np.sqrt(
+    #         np.nanstd(flux_arr, axis=0) ** 2.0 + np.nanmean(fluxerr_arr, axis=0) ** 2.0
+    #     )
+    # else:
+    #     avg_fluxerr = np.nanmean(fluxerr_arr, axis=0)
+
+    # TODO: Check how to save the total flux properly as only one dataset
+    # outhdul["OI_FLUX"].data = outhdul["OI_FLUX"].data[0:1]
+    # outhdul["OI_FLUX"].data["FLUXDATA"] = avg_flux
+    # outhdul["OI_FLUX"].data["FLUXERR"] = avg_fluxerr
+
+
+if __name__ == "__main__":
+    ...
