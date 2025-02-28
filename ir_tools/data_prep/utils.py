c820b2eac7b6aac8c9524a850fe85cafe2df9514
M. B. Scheuck <programming.mbsck@mailbox.org>
2025-02-28 13:21:13 +0100

    Update `average_flux`

--- ir_tools/data_prep/utils.py
+++ ir_tools/data_prep/utils.py
@@ -1,8 +1,10 @@
+import shutil
 from pathlib import Path
 from typing import List
 
 import numpy as np
-from astropy.stats import biweight_location
+from astropy.io import fits
+from astropy.stats import biweight_location, biweight_midvariance
 
 
 # TODO: For io use my io script
@@ -27,38 +29,35 @@ def average_flux(
         fits_files = [fits_files]
 
     if method == "biweight":
-        avgfunc = biweight_location
+        avgfunc, avgvar = biweight_location, biweight_midvariance
     elif method == "mean":
         avgfunc = np.nanmean
     else:
         avgfunc = np.nanmedian
 
-    flux, flux_err = [], []
+    flux, fluxerr = [], []
     for fits_file in fits_files:
         with fits.open(fits_file) as hdul:
             flux.append(hdul["oi_flux"].data["fluxdata"])
-            flux_err.append(hdul["oi_flux"].data["fluxerr"])
+            fluxerr.append(hdul["oi_flux"].data["fluxerr"])
 
-    flux, flux_err = np.array(flux), np.array(flux_err)
+    flux, fluxerr = np.array(flux), np.array(fluxerr)
+    if len(fits_files) <= 1:
+        flux, fluxerr = flux[0], fluxerr[0]
 
-    if len(fits_files) > 1:
-        avg_flux = avgfunc(flux, axis=0)
-    else:
-        avg_flux = avgfunc(flux)
+    avg_flux = avgfunc(flux, axis=0)
+    avg_fluxerr = np.sqrt(avgvar(flux, axis=0) + np.nanmean(fluxerr, axis=0)**2.0)
 
-    # TODO: Make sure the error calculation here is correct
-    # if len(flux_arr) > 3:
-    #     avg_fluxerr = np.sqrt(
-    #         np.nanstd(flux_arr, axis=0) ** 2.0 + np.nanmean(fluxerr_arr, axis=0) ** 2.0
-    #     )
-    # else:
-    #     avg_fluxerr = np.nanmean(fluxerr_arr, axis=0)
-
-    # TODO: Check how to save the total flux properly as only one dataset
-    # outhdul["OI_FLUX"].data = outhdul["OI_FLUX"].data[0:1]
-    # outhdul["OI_FLUX"].data["FLUXDATA"] = avg_flux
-    # outhdul["OI_FLUX"].data["FLUXERR"] = avg_fluxerr
+    if save_dir:
+        shutil.copy(fits_files[0], save_dir)
+        with fits.open(save_dir, mode="update") as outhdul:
+            outhdul["oi_flux"].data = outhdul["oi_flux"].data[0:1]
+            outhdul["oi_flux"].data["fluxdata"] = avg_flux
+            outhdul["oi_flux"].data["fluxerr"] = avg_fluxerr
+            outhdul.flush()
 
 
 if __name__ == "__main__":
-    ...
+    data_dir = Path().home() / "Data" / "reduced" / "HD_142527" / "matisse"
+    fits_file = list(data_dir.glob("*.fits"))[0]
+    average_flux(fits_file, save_dir=data_dir / f"{fits_file.stem}_AVG_FLUX.fits")
